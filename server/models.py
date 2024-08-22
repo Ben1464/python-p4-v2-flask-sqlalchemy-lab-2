5eecc2c54e1e5ead75eac9472b635a644fc1d3ba
Ben <kimaniben14@gmail.com>
2024-08-22 10:17:11 +0100

    Still working on it

--- server/models.py
+++ server/models.py
@@ -1,32 +1,47 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
+db = SQLAlchemy()
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-
-db = SQLAlchemy(metadata=metadata)
-
-
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
 
-    def __repr__(self):
-        return f'<Customer {self.id}, {self.name}>'
+    # Define the relationship to Review
+    reviews = db.relationship('Review', back_populates='customer')
 
+    # Establish an association proxy for items through reviews
+    items = association_proxy('reviews', 'item')
 
-class Item(db.Model):
+    # Serialization rule to avoid recursion
+    serialize_rules = ('-reviews.customer',)
+
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    price = db.Column(db.Float)
+    name = db.Column(db.String, nullable=False)
 
-    def __repr__(self):
-        return f'<Item {self.id}, {self.name}, {self.price}>'
+    # Define the relationship to Review
+    reviews = db.relationship('Review', back_populates='item')
+
+    # Serialization rule to avoid recursion
+    serialize_rules = ('-reviews.item',)
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String, nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'), nullable=False)
+
+    # Establish relationships with Customer and Item
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
+
+    # Serialization rule to avoid recursion
+    serialize_rules = ('-customer.reviews', '-item.reviews')
